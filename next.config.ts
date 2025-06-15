a481ba0671c310960d3c027916221c214178badc
NAWTi <macbookpro@MacBooks-MacBook-Pro.local>
2025-06-15 15:51:17 +0100

    standalone folder

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone", // Best for Render/Node.js hosting
 };
 
 export default nextConfig;
