962279fdf0267d4284fa1fbe50a308bcd49a334d
NAWTi <macbookpro@MacBooks-MacBook-Pro.local>
2025-06-15 16:02:02 +0100

    out export

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "standalone", // Best for Render/Node.js hosting
+  output: "export", // Best for Render/Node.js hosting
 };
 
 export default nextConfig;
